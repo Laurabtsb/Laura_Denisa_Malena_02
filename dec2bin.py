7177bec6dbba03699dcc3eefa4f24cfdfea65a5d
DenisaIusan <90776670+DenisaIusan@users.noreply.github.com>
2021-09-29 16:27:56 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -4,6 +4,9 @@
 # El binario resultante es un string e.g. "101"
 # Se usa la función bin() que transforma e.g. 3 en "0b11".
 # En esta función se quita el "0b" para dejar el "11"
+
+## MODIFICADO POR DENISA BAJO LA SUPREMACÍA DE LAURA
+
 # ------------------------------------------------------
 def dec2bin(numero_decimal, numero_bits):
     numero_binario = bin(numero_decimal)
